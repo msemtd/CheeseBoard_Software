c6458167327ca3a1ef90121043e6aabc66d341a8
Matthew Gates <matthewg42@gmail.com>
2017-10-19 13:03:18 +0100

    starting to add rotary to example

--- examples/NetworkScanner/NetworkScanner.ino
+++ examples/NetworkScanner/NetworkScanner.ino
@@ -12,11 +12,28 @@
 // Include CheeseBoard headers for the components we'll be using in
 // this example
 #include <CbDebug.h>
+#include <CbMillis.h>
 #include <CbOled.h>
 #include <GfxSSIDListBox.h>
+#include <CbRotaryInput.h>
 
 // A global listbox object
 GfxSSIDListBox listbox(128);
+
+uint32_t lastDisplayRefresh = 0;
+bool displayUpdate = true;
+
+void buttonCb(void)
+{
+    DBLN("buttonCb");
+    displayUpdate = true;
+}
+
+void rotaryCb(int8_t diff, int32_t value)
+{
+    DBF("rotaryCb diff=%d value=%d\n", diff, value);
+    displayUpdate = true;
+}
 
 void setup()
 {
@@ -24,6 +41,7 @@ void setup()
     DBLN(F("S:setup"));
 
     CbOled.begin();
+    CbRotaryInput.begin(buttonCb, rotaryCb);
 
     listbox.add(GfxNetInfo(String("wirescubedextended"), 1, 75));
     listbox.add(GfxNetInfo(String("leslians"), 3, 53));
@@ -38,9 +56,14 @@ void setup()
 
 void loop()
 {
-    CbOled.clearBuffer();
-    listbox.draw();
-    CbOled.sendBuffer();
-    delay(500);
+    CbRotaryInput.update();
+
+    if (CbMillis() > lastDisplayRefresh + 5000 || displayUpdate) {
+        CbOled.clearBuffer();
+        listbox.draw();
+        CbOled.sendBuffer();
+        displayUpdate = false;
+    }
+
 }
 

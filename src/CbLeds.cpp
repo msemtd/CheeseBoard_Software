52dcc9137b9ced9f418ce5eb973b52ca42c2d0e9
Matthew Gates <matthewg42@gmail.com>
2017-10-21 09:05:32 +0100

    consolidate CbNeoPixel into CbLeds, remove obsolete CbRawButton

--- src/CbLeds.cpp
+++ src/CbLeds.cpp
@@ -1,5 +1,20 @@
+#include <Millis.h>
 #include "CbLeds.h"
 #include "Config.h"
 
 CbNeoPixel CbLeds(RGBLED_COUNT, RGBLED_DATA_PIN, RGBLED_TYPE);
+
+CbNeoPixel::CbNeoPixel(uint16_t n, uint8_t p, neoPixelType t) :
+    Adafruit_NeoPixel(n, p, t)
+{
+}
+
+void CbNeoPixel::show()
+{
+    Adafruit_NeoPixel::show();
+    addMillisOffset(numPixels() * 0.0215);
+}
+
+
+
 

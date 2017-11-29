dcc3084f8272faec3dffccf33d1de12e6ee6fe95
Matthew Gates <matthewg42@gmail.com>
2017-11-29 18:13:43 +0000

    use prototypes to have a sane order in main

--- examples/KnightRider/KnightRider.ino
+++ examples/KnightRider/KnightRider.ino
@@ -25,38 +25,16 @@
 // Include files from this example
 #include "KnightRiderEffect.h"
 
+// Function prototypes.  For the Arduino IDE you don't need these, but they 
+// make it possible to build with the Makefile approach without having to put
+// functions in the file before they are referenced by other functions.
+void setup();
+void loop();
+void buttonCb(uint16_t durationMs);
+void rotaryCb(int8_t diff, int32_t value);
+
+// Global variables
 bool twisted = false;
-
-void rotaryCb(int8_t diff, int32_t value)
-{
-    DBF("rotaryCb diff=%d value=%d\n", diff, value);
-    twisted = true;
-    if (CbRotaryInput.buttonPushed()) {
-        // If we push and hold while twisting, adjust the brightness
-        if (diff > 0) {
-            KnightRiderEffect.brighter();
-        } else {
-            KnightRiderEffect.dimmer();
-        }
-    } else {
-        // Otherwise, adjust the frequency of the effect
-        if (diff > 0) {
-            KnightRiderEffect.setFrequency(KnightRiderEffect.getFrequency() * 1.15);
-        } else {
-            KnightRiderEffect.setFrequency(KnightRiderEffect.getFrequency() / 1.15);
-        }
-    }
-}
-
-void buttonCb(uint16_t durationMs)
-{
-    DBF("buttonCb() - durationMs=%d ", durationMs);
-    if (!twisted) {
-        KnightRiderEffect.changeColor();
-        DBLN("changing color");
-    }
-    twisted = false;
-}
 
 void setup()
 {
@@ -85,5 +63,36 @@ void loop()
 {
     KnightRiderEffect.update();
     CbRotaryInput.update();
+}
+
+void buttonCb(uint16_t durationMs)
+{
+    DBF("buttonCb() - durationMs=%d ", durationMs);
+    if (!twisted) {
+        KnightRiderEffect.changeColor();
+        DBLN("changing color");
+    }
+    twisted = false;
+}
+
+void rotaryCb(int8_t diff, int32_t value)
+{
+    DBF("rotaryCb diff=%d value=%d\n", diff, value);
+    twisted = true;
+    if (CbRotaryInput.buttonPushed()) {
+        // If we push and hold while twisting, adjust the brightness
+        if (diff > 0) {
+            KnightRiderEffect.brighter();
+        } else {
+            KnightRiderEffect.dimmer();
+        }
+    } else {
+        // Otherwise, adjust the frequency of the effect
+        if (diff > 0) {
+            KnightRiderEffect.setFrequency(KnightRiderEffect.getFrequency() * 1.15);
+        } else {
+            KnightRiderEffect.setFrequency(KnightRiderEffect.getFrequency() / 1.15);
+        }
+    }
 }
 
